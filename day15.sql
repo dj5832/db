cbf8254b4380491b7cdabf440623fcbd18798789
dj5832 <126127708+dj5832@users.noreply.github.com>
2023-05-13 23:06:32 +0900

    23

---
+++ day15.sql
@@ -0,0 +1,148 @@
+[ GROUP FUNCTION 응용, 확장 ];
+select deptno, sum(sal)
+  FROM emp 
+ GROUP BY deptno
+UNION
+select NULL, sum(sal)
+  FROM emp
+ ORDER BY deptno DESC NULLS LAST;
+ 
+ROLLUP - 1. GROUP BY 의 확장 구문
+		 2. 정해진 규칙으로 서브 그룹을 생성하고 생성된 서브 그룹을 하나의 집합으로 변환
+		 3. GROUP BY ROLLUP(col1, col2...)
+		 4. ROLLUP 절에 기술된 컬럼을 오른쪽에서부터 하나씩 제거해 가며 서브그룹을 생성
+		 	ROLLUP 의 경우 방향성이 있기 때문에 컬럼 기술 순서가 다르면 다른 결과가 나온다.
+		 	
+예시 - GROUP BY ROLLUP (deptno)
+1. GROUP BY deptno ==> 부서번호별 총계
+2. GROUP BY '' ==> 전체 총계
+
+예시 - GROUP BY ROLLUP (job, deptno)
+1. GROUP BY job, deptno ==> 담당업무, 부서번호별 총계
+2. GROUP BY job		==> 담당업무별 총계
+3. GROUP BY ''		==> 전체 총계
+
+select job, deptno, sum(sal + nvl(comm, 0)) sal
+  FROM emp
+ GROUP BY ROLLUP (job, deptno);
+ 
+select *
+  FROM emp;
+  
+* ROLLUP 절에 N개의 컬럼을 기술 했을 때 SUBGROUP 의 개수는 N + 1 개의 서브 그룹이 생성
+
+GROUPING 함수
+GROUPING(column)
+
+job 컬럼이 소계 계산으로 사용되어 NULL 값이 나온 것인지, 정말 컬럼의 값이 NULL 인 행들이
+GROUP BY 된 것인지 알려면 GROUPING 함수를 사용해야 정확한 값을 알 수 있다.
+
+select nvl(job, '총계'), deptno, sum(sal + nvl(comm, 0)) sal, GROUPING(job), GROUPING(deptno)
+  FROM emp
+ GROUP BY ROLLUP (job, deptno);
+ 
+GROUPING(column) - 0, 1
+0 - 컬럼이 소계 계산에 사용되지 않았다. (GROUP BY 컬럼으로 사용됨)
+1 - 컬럼이 소계 계산에 사용 되었다.
+
+select decode(GROUPING(job), 1, '총계', job) job, decode(GROUPING(deptno), 1, '소계', deptno) deptno, GROUPING(job), GROUPINg(deptno), sum(sal + nvl(comm , 0)) sal
+  FROM emp
+ GROUP BY ROLLUP (job, deptno);
+ 
+-- 실습1
+
+select decode(GROUPING(job), 1, '총', job) job,
+	   decode(GROUPING(deptno) + GROUPING(job), 2,'계', 1, '소계', deptno) deptno,
+	   GROUPING(job), GROUPING(deptno), sum(sal + nvl(comm ,0)) sal
+	     FROM emp
+ GROUP BY ROLLUP (job, deptno);
+-- 실습2
+	   
+SELECT deptno, job, sum(sal + nvl(comm, 0)) sal
+  FROM emp
+ GROUP BY ROLLUP (deptno, job);
+
+-- 실습3
+
+select dname, job, sum(sal + nvl(e.comm, 0)) sal
+  FROM dept d, emp e
+ where d.deptno = e.deptno 
+ GROUP BY ROLLUP (d.dname, job)
+ ORDER BY dname , job desc;
+ 
+--------------------------------------------------------------------------------------------------------------------------------------
+확장된 GROUP BY 
+1. ROLLUP - 컬럼 기술에 방향성이 존재
+	GROUP BY ROLLUP (job, deptno) != GROUP BY ROLLUP (deptno, job)
+	GROUP BY job, deptno			 GROUP BY deptno, job
+	GROUP BY job					 GROUP BY deptno
+	GROUP BY ''						 GROUP BY ''
+	단점 - 개발자가 필요가 없는 서브그룹을 임의로 제거할 수 없다.
+
+2. GROUPING SETS - 필요한 서브그룹을 임의로 지정하는 형태
+	==> 복수의 GROUP BY 를 하나도 합쳐서 결과를 돌려주는 형태
+	GROUP BY GROUPING SETS (col1, col2)
+	
+	GROUP BY col1
+	union all 
+	GROUP BY col2
+	
+	GROUP BY GROUPING SETS (col2, col1)
+	
+	GROUP BY col2
+	union all 
+	GROUP BY col1
+	
+	GROUPING SETS 의 경우 ROLLUP 과는 다르게 컬럼 나열순서가 데이터 자체에 영향을 미치지 않는다.
+	
+	복수컬럼으로 GROUP BY 
+	GROUP BY col1, col2
+	UNION ALL 
+	GROUP BY col1
+	==> GROUPING SETS ((col1, col2), col1)
+	
+GROUPING SETS 실습
+
+SELECT job, deptno, sum(sal + nvl(comm , 0)) sal
+  FROM emp
+ GROUP BY GROUPING SETS (job, deptno);
+ 
+위 쿼리를 UNION ALL 로 풀어쓰기
+
+select job, NULL, sum(sal + nvl(comm , 0)) sal
+  FROM emp
+ GROUP BY job 
+ 
+ UNION ALL 
+ select deptno, NULL, sum(sal + nvl(comm , 0)) sal
+  FROM emp
+ GROUP BY deptno;
+
+
+3. CUBE
+GROUP BY 절을 확장한 구문
+CUBE 절에 나열한 모든 가능한 조합으로 서브그룹을 생성
+GROUP BY CUBE (job, depno)
+
+GROUP BY job, deptno
+GROUP BY job
+GROUP BY deptno
+GROUP BY ''
+
+select job, deptno, GROUPING(job), GROUPING(deptno), sum(sal + nvl(comm, 0)) sal
+  FROM emp
+ GROUP BY CUBE (job, deptno);
+
+select job, sum(sal + nvl(comm, 0)) sal
+  FROM emp
+ GROUP BY ROLLUP(job);
+
+
+CUBE 의 경우 기술한 컬럼으로 모든 가능한 조합으로 서브그룹을 생성하낟.
+가능한 서브구릅은 2^기술한 컬럼갯수
+기술한 컬럼이 3ㄱ만 넘어도 생성되는 서브그룹의 개수가 8개가 넘기 때문에 실제 필요하지 않은 서브그룹이
+포함될 가능성이 높다 ==> ROLLUP, GROUPING SETS 보다 활용성이 떨어진다.
+
+GROUP BY ROLLUP (job, deptno, mgr)
+GROUP BY CUBE   (job, deptmo, mgr)
+
---
+++ day17.sql
@@ -0,0 +1,101 @@
+select min(decode(d, 1, dt, NULL)) sun, min(decode(d, 2, dt, NULL)) mon , 
+	   min(decode(d, 3, dt, NULL)) tue, min(decode(d, 4, dt, NULL)) wed ,min(decode(d, 5, dt, NULL)) thu 
+	 , min(decode(d, 6, dt, NULL)) fri, min(decode(d, 7, dt, NULL)) sat
+FROM (
+		SELECT 
+				to_date(:yyyymm,'YYYYMM') + (LEVEL - 1) dt
+				,to_char(to_date(:yyyymm,'YYYYMM') + (LEVEL - 1), 'D') d
+				,to_char(to_date(:yyyymm,'YYYYMM') + (LEVEL - 1), 'WW') ww
+		  FROM dual  
+		 CONNECT BY LEVEL <= to_char(last_day(to_date(:yyyymm, 'YYYYMM')), 'DD')
+		 )
+ GROUP BY ww
+ ORDER BY ww
+;
+
+SELECT to_date(:yyyymmdd1, 'YYYYMMDD') - to_date(:yyyymmdd2, 'YYYYMMDD') d 
+  FROM dual;
+
+SELECT 
+	   MIN(DECODE(d, 1, dt, NULL)) sun, MIN(DECODE(d, 2, dt, NULL)) mon,
+	   MIN(DECODE(d, 3, dt, NULL)) tue, MIN(DECODE(d, 4, dt, NULL)) wed,
+	   MIN(DECODE(d, 5, dt, NULL)) thu, MIN(DECODE(d, 6, dt, NULL)) fri,
+	   MIN(DECODE(d, 7, dt, NULL)) sat
+  FROM (
+  			SELECT DECODE(TO_CHAR(TO_DATE(:yyyymm, 'YYYYMM'), 'D'), 1, TO_DATE(:yyyymm, 'YYYYMM'), NEXT_DAY(LAST_DAY(TO_DATE(:yyyymm, 'YYYYMM') - 1) - 7, 1)) + (LEVEL - 1) dt,
+				   TO_CHAR(DECODE(TO_CHAR(TO_DATE(:yyyymm, 'YYYYMM'), 'D'), 1, TO_DATE(:yyyymm, 'YYYYMM'), NEXT_DAY(LAST_DAY(TO_DATE(:yyyymm, 'YYYYMM') - 1) - 7, 1)) + (LEVEL - 1), 'D') d,
+				   TO_CHAR(DECODE(TO_CHAR(TO_DATE(:yyyymm, 'YYYYMM'), 'D'), 1, TO_DATE(:yyyymm, 'YYYYMM'), NEXT_DAY(LAST_DAY(TO_DATE(:yyyymm, 'YYYYMM') - 1) - 7, 1)) + (LEVEL - 1), 'IW') iw
+			  FROM dual
+			CONNECT BY LEVEL <= DECODE(TO_CHAR(LAST_DAY(TO_DATE(:yyyymm, 'YYYYMM')), 'D'), 7, LAST_DAY(TO_DATE(:yyyymm, 'YYYYMM')), NEXT_DAY(LAST_DAY(TO_DATE(:yyyymm, 'YYYYMM')), 7))
+							  - DECODE(TO_CHAR(TO_DATE(:yyyymm, 'YYYYMM'), 'D'), 1, TO_DATE(:yyyymm, 'YYYYMM'), NEXT_DAY(LAST_DAY(TO_DATE(:yyyymm, 'YYYYMM') - 1) - 7, 1)) + 1
+  		) calendar
+ GROUP BY DECODE(d, 1, decode(iw, 52, 1, 53, 1, iw + 1), iw)
+ ORDER BY sat
+;
+
+---------------------------------------------------------------------------------------------------------------------------------------------------
+계층쿼리의 SELECT 퀴리의 실행순서 FROM -> START WITH, CONNECT BY -> WHERE 
+일반쿼리의 SELECT 퀴리의 실행순서 FROM -> WHERE -> SELECT -> (GROUP BY) -> ORDER BY
+
+계층쿼리에서 조회할 행의 조건을 기술할 수 있는 부분이 2곳 존재
+1. WHERE - START WITH, CONNECT BY 에 의해 조회된 행을 대상으로 적용
+2. CONNECT BY - 다음 행으로 연결할지, 말지를 결정
+
+SELECT lpad(' ', (LEVEL - 1) * 4) || deptnm deptnm
+  FROM dept_h
+ START WITH deptcd = 'dept0'
+ CONNECT BY PRIOR deptcd = p_deptcd AND deptnm != '정보기획부'
+;
+
+SELECT lpad(' ', (LEVEL - 1) * 4) || deptnm deptnm
+  FROM dept_h
+ WHERE deptnm != '정보기획부'
+ START WITH deptcd = 'dept0'
+ CONNECT BY PRIOR deptcd = p_deptcd
+;
+
+--------------------------------------------------------------------------------------------------------------
+
+[ 계층쿼리에서 사용할 수 있는 특수함수 ]
+
+CONNECT_BY_ROOT(col) - 최상위 행의 'col' 컬럼의 값
+SYS_CONNECT_BY_PATH(col, 구분자) - 계층의 순회경로를 표현(각 계층을 지나면서 어떤 컬럼을 지나 왔는지 보여줌)
+CONNECT_BY_ISLEAF - 해당 행이 LEAF 노드(1)인지 아닌지(0)를 반환(최하단 노드인지 아닌지를 구별해서 알려준다.)
+
+SELECT deptcd, p_deptcd, lpad(' ', (LEVEL - 1) * 4) || deptnm deptnm,
+		CONNECT_BY_ROOT(deptnm),
+		ltrim(sys_connect_by_path(deptnm, '-'), '-'),
+		CONNECT_BY_ISLEAF
+  FROM dept_h
+ START WITH deptcd = 'dept0'
+ CONNECT BY PRIOR deptcd = p_deptcd
+;
+
+SELECT seq, lpad(' ', (LEVEL - 1)* 4) || title title
+  FROM board_test
+ START WITH parent_seq IS NULL
+ CONNECT BY PRIOR seq = parent_seq
+ORDER BY seq DESC;
+ 
+select *
+  FROM board_test;
+--------------------------------------------------------------
+ ALTER TABLE board_test ADD (gn NUMBER);
+
+UPDATE board_test SET gn = 4
+WHERE seq IN (4, 5, 6, 7, 8, 10, 11);
+
+UPDATE board_test SET gn = 1
+WHERE seq IN (1, 9);
+
+UPDATE board_test SET gn = 2
+WHERE seq IN (2, 3);
+
+select *
+  FROM board_test;
+  
+SELECT seq, gn, CONNECT_BY_ROOT(seq) S_GN ,parent_seq, lpad(' ', (LEVEL - 1)* 4) || title title
+  FROM board_test
+ START WITH parent_seq IS NULL 
+ CONNECT BY PRIOR seq = parent_seq
+ ORDER BY gn DESC, seq;
